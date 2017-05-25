0c67f3c663b87ce5d4c95ad20c3f8160c0a866a0
Steve Kinney <hello@stevekinney.net>
2017-05-25 06:14:04 -0600

    Make some helper methods

--- your-stuff/index.js
+++ your-stuff/index.js
@@ -1,22 +1,21 @@
 import Database from "../my-stuff/super-real-totally-not-fake-database";
 
+const map = (collection, callback) => Promise.all(collection.map(callback));
+const toObject = artists => ({ artists });
+
 const getAlbumsForArtist = artist => {
   return new Promise((resolve, reject) => {
-    Promise.all(
-      artist.albums.map(id => Database.findAlbum(id))
-    ).then(albums => {
+    map(artist.albums, Database.findAlbum).then(albums => {
       resolve({ ...artist, albums });
     });
   });
 };
 
-const getAlbumsForArtists = artists => {
-  return Promise.all(artists.map(getAlbumsForArtist));
-};
+const getAlbumsForArtists = artists => map(artists, getAlbumsForArtist);
 
 const getSongsForAlbum = album => {
   return new Promise((resolve, reject) => {
-    Promise.all(album.songs.map(id => Database.findSong(id))).then(songs => {
+    map(album.songs, Database.findSong).then(songs => {
       resolve({ ...album, songs });
     });
   });
@@ -24,17 +23,13 @@ const getSongsForAlbum = album => {
 
 const getSongsForArtist = artist => {
   return new Promise((resolve, reject) => {
-    Promise.all(artist.albums.map(getSongsForAlbum)).then(albums => {
+    map(artist.albums, getSongsForAlbum).then(albums => {
       resolve({ ...artist, albums });
     });
   });
 };
 
-const getSongsForArtists = artists => {
-  return Promise.all(artists.map(getSongsForArtist));
-};
-
-const toObject = artists => ({ artists });
+const getSongsForArtists = artists => map(artists, getSongsForArtist);
 
 export default () => {
   return new Promise((resolve, reject) => {
